316524ec52a1608d0fd2c39af1a9442b6ff74135
Benito Vargas <benito.vargas@grupo-ortiz.com>
2023-03-07 15:43:12 -0600

    Creando la API

---
+++ ms_bruckner/app/Models/Unidad.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Unidad extends Model
+{
+            use HasFactory;
+            use SoftDeletes;
+
+            protected $table = 'unidades';
+
+            public function marca( ){
+                return $this->belongsTo( Marca::class, 'marca_id', 'id');
+                //return $this->hasOne(Marca::class);
+        }
+
+        public function tipo( ){
+            return $this->belongsTo( Tipo::class,  'tipo_id','id');
+            //return $this->hasOne(Tipo::class);
+        }
+}
--- ms_bruckner/routes/api.php
+++ ms_bruckner/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\unidadesController as Unidades;
+
+//use App\Http\Controllers\Unidad as Unidades;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('unidades', Unidades::class);
+/*
+Route::get('/unidades', function () {
+    echo "perrurry";
+    
+    return response()->json([
+        'name' => 'Abigail',
+        'state' => 'CA',
+    ]);
+    
+    php artisan make:controller unidadesController --api
+});
+*/
