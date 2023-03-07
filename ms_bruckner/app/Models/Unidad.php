a81ddd4f9d0421c75d98b02e938fc8bb3af9d200
Animus <a1140252x@gmail.com>
2023-03-07 16:06:34 -0600

    api methods
    creacion de los metodos en unida_tipos

--- ms_bruckner/app/Models/Unidad.php
+++ ms_bruckner/app/Models/Unidad.php
@@ -6,21 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+class Unidad extends Model {
+    use HasFactory;
+    use SoftDeletes;
 
-class Unidad extends Model
-{
-            use HasFactory;
-            use SoftDeletes;
+    protected $table = 'unidades';
 
-            protected $table = 'unidades';
+    public function marca( ){
+        return $this->belongsTo( Marca::class, 'marca_id', 'id');
+        //return $this->hasOne(Marca::class);
+    }
 
-            public function marca( ){
-                return $this->belongsTo( Marca::class, 'marca_id', 'id');
-                //return $this->hasOne(Marca::class);
-        }
-
-        public function tipo( ){
-            return $this->belongsTo( Tipo::class,  'tipo_id','id');
-            //return $this->hasOne(Tipo::class);
-        }
+    public function tipo( ){
+        return $this->belongsTo( Tipo::class,  'tipo_id','id');
+        //return $this->hasOne(Tipo::class);
+    }
 }
